327056eb20e96a3ba5f27a24ae41a948a118a519
Виталий <wental-@mail.ru>
2019-09-19 00:47:00 +0300

    static properties and methods

--- classes/Car.php
+++ classes/Car.php
@@ -8,16 +8,23 @@ class Car
     public $speed;
     public $brand;
     
+    const TEST_CAR = "Прототип";
+    const TEST_CAR_SPEED = 300;
+    
+    public static $countCar = 0;
+    
     public function __construct($brand, $color, $speed = 180, $wheels = 4)
     {
         $this->color = $color;
         $this->wheels = $wheels;
         $this->speed = $speed;
         $this->brand = $brand;
+        self::$countCar++;
     }
     
     public function __destruct()
     {
+        echo "Работает деструктор: ";
         var_dump($this);
         echo "<br>";
     }
@@ -29,6 +36,18 @@ class Car
              . "Цвет кузова: {$this->color} <br>"
              . "Количество колёс: {$this->wheels} <br>"
              . "Максимальная скорость: {$this->speed} км/ч <br>";
+    }
+    
+    public static function getCount() 
+    {
+        return self::$countCar;
+    }
+    
+    public function getPrototypeInfo()
+    {
+        return "<b>Информация о тестовом автомобиле:</b><br>"
+             . "Марка автомобиля: " . self::TEST_CAR . "<br>"
+             . "Максимальная скорость: " . self::TEST_CAR_SPEED . " км/ч <br>";
     }
     
 }
--- index.php
+++ index.php
@@ -7,9 +7,18 @@ function debug($data)
     echo "<pre>" . print_r($data) . "</pre>";
 }
 
+echo Car::$countCar . "<br>";
+
 $car1 = new Car('BMW', 'чёрный');
+
+echo Car::$countCar . "<br>";
 
 $car2 = new Car('Mazda', 'белый', 220);
 
+echo Car::getCount() . "<br>";
+
 echo $car1->getCarInfo();
 echo $car2->getCarInfo();
+
+echo $car1->getPrototypeInfo();
+echo Car::TEST_CAR_SPEED . "<br>";
