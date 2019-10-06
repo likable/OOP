cbb5e1c65fc364d06c5a300338c7ee46e4be6eae
Виталий <wental-@mail.ru>
2019-10-06 23:57:27 +0300

    autoload

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
 
 error_reporting(-1);
+/*
 require_once 'classes/Product.php';
 require_once 'classes/I3D.php';
 require_once 'classes/IGadget.php';
 require_once 'classes/NotebookProduct.php';
 require_once 'classes/BookProduct.php';
+*/
+
+function autoloader1($class)
+{
+    $file = __DIR__ . "/classes/{$class}.php";
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+function autoloader2($class)
+{
+    $file = __DIR__ . "/classes/interfaces/{$class}.php";
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+
+spl_autoload_register("autoloader1");
+spl_autoload_register("autoloader2");
 
 function debug($data)
 {
