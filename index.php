56d387008926835f10e0fb50296dabeb7710527c
Виталий <wental-@mail.ru>
2019-10-11 23:52:01 +0300

    composer install

--- index.php
+++ index.php
@@ -1,56 +1,36 @@
 <?php
 
 use classes\interfaces\IGadget;
-use classes\BookProduct;
-use classes\NotebookProduct;
+use classes\{BookProduct, NotebookProduct};
 
 error_reporting(-1);
-/*
-require_once 'classes/Product.php';
-require_once 'classes/I3D.php';
-require_once 'classes/IGadget.php';
-require_once 'classes/NotebookProduct.php';
-require_once 'classes/BookProduct.php';
-*/
 
-function autoloader1($class)
-{
-    $class = str_replace("\\", "/", $class);
-    $file = __DIR__ . "/{$class}.php";
-    if (file_exists($file)) {
-        require_once $file;
-    }
-}
-//function autoloader2($class)
+require_once __DIR__ . "/vendor/autoload.php";
+
+//function autoloader1($class)
 //{
-//    $file = __DIR__ . "/classes/interfaces/{$class}.php";
+//    $class = str_replace("\\", "/", $class);
+//    $file = __DIR__ . "/{$class}.php";
 //    if (file_exists($file)) {
 //        require_once $file;
 //    }
 //}
-
-spl_autoload_register("autoloader1");
-//spl_autoload_register("autoloader2");
+//
+//spl_autoload_register("autoloader1");
 
 function debug($data)
 {
     echo "<pre>" . print_r($data, 1) . "</pre>";
 }
 
-
 function offerCase(IGadget $product){
     echo "<p>Предлагаем чехол для гаджета {$product->getName()}</p>";
 }
 
-
 $book = new BookProduct("Три мушкетёра", 20, 1000);
 $notebook = new NotebookProduct("Dell", 1000, "Intel");
 
-//var_dump($notebook instanceof IGadget);
-
 offerCase($notebook);
-//offerCase($book);
-
 
 debug($book);
 
